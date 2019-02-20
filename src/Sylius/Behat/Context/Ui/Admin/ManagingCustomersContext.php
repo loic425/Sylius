8d7b871e940ccc4a015d09d08c1f01d20f37cc3f
Loïc Frémont <lc.fremont@gmail.com>
2019-02-20 14:39:39 +0100

    autowire ui contexts

--- src/Sylius/Behat/Context/Ui/Admin/ManagingCustomersContext.php
+++ src/Sylius/Behat/Context/Ui/Admin/ManagingCustomersContext.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace Sylius\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Context\Context;
-use Sylius\Behat\Page\Admin\Crud\IndexPageInterface;
+use Sylius\Behat\Page\Admin\Customer\IndexPageInterface;
 use Sylius\Behat\Page\Admin\Customer\CreatePageInterface;
 use Sylius\Behat\Page\Admin\Customer\IndexPageInterface as CustomerIndexPageInterface;
 use Sylius\Behat\Page\Admin\Customer\ShowPageInterface;
